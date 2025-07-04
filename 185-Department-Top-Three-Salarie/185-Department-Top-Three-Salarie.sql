47c56d7c5015e1ed6914483dc0a490d499f672cc
Siddhesh Chaudhari <siddheshavic67@gmail.com>
2025-07-04 21:23:35 +0530

    Day 22 SQL-Top-50

---
+++ 185-Department-Top-Three-Salarie/185-Department-Top-Three-Salarie.sql
@@ -0,0 +1,9 @@
+SELECT d.name AS Department, e1.name AS Employee, e1.salary AS Salary
+FROM Employee e1 INNER JOIN Department d
+ON e1.departmentId = d.id
+WHERE 3 > (
+    SELECT COUNT(DISTINCT (e2.Salary))
+    FROM Employee e2
+    WHERE e2.Salary > e1.Salary AND
+    e1.DepartmentId = e2.DepartmentId
+)
